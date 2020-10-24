3f536c84df37aae7330e7b53b5106aa9cf5d270c
JoshuaValey <j97valey@gmail.com>
2020-10-23 18:28:27 -0600

    Refactor de reconstruirDiccionario

--- Laboratorio4ED2/CompresorLZW/Estructuras/Descompresor.cs
+++ Laboratorio4ED2/CompresorLZW/Estructuras/Descompresor.cs
@@ -66,8 +66,46 @@ namespace CompresorLZW.Estructuras
             int flasg = 0;
         }*/
 
-        public Dictionary<int, string> reconstruirDiccionario(Dictionary<string, int> original, List<int> numeros)
+
+        public string reconstruirDic(Dictionary<string, int> original, List<int> numeros)
         {
+            Dictionary<int, string> newDicc = new Dictionary<int, string>();
+
+            foreach (var item in original)
+            {
+                newDicc.Add(item.Value, item.Key);
+            }
+            int ultimaPos = newDicc.Count;
+
+
+            int posAnterior = numeros[0];
+            int posSiguiente = 0;
+            string indice = newDicc[posAnterior];
+            string cadenaAuxiliar = "";
+            string mensaje = indice;
+
+            int i = 1;
+            while (i < numeros.Count)
+            {
+                posSiguiente = numeros[i];
+
+                if (newDicc.ContainsKey(posSiguiente))
+                    cadenaAuxiliar = newDicc[posSiguiente];
+                else //Este caso sale cuando hay muchos caracteres iguales repetidos uno detras de otro....
+                    cadenaAuxiliar = newDicc[posAnterior] + indice;
+
+                mensaje += cadenaAuxiliar;
+                indice = cadenaAuxiliar.Substring(0, 1);
+                newDicc.Add(++ultimaPos, newDicc[posAnterior] + indice);
+                posAnterior = posSiguiente;
+                i++;
+            }
+            return mensaje;
+        }
+        /*public Dictionary<int, string> reconstruirDiccionario(Dictionary<string, int> original, List<int> numeros)
+        {
+
+            reconstruirDic(original, numeros);
             Dictionary<int, string> originalI = new Dictionary<int, string>();
             Dictionary<int, string> agregados = new Dictionary<int, string>();
             string anterior = "";
@@ -94,7 +132,7 @@ namespace CompresorLZW.Estructuras
                 }
                 //|||||||VVVVVVVVV||||||||
                 //numero ya esta
-                if (original.ContainsKey(anterior + actual) || agregados.ContainsValue(anterior+actual))
+                if (original.ContainsKey(anterior + actual) || agregados.ContainsValue(anterior + actual))
                 {
                     anterior += actual;
                     actual = "";
@@ -117,7 +155,7 @@ namespace CompresorLZW.Estructuras
             }
 
             return newDic;
-        }
+        }*/
 
 
     }
