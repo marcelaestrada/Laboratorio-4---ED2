654e8f4e8dde870d0fb2ca03ea5f8523cef04ce4
marcelaestradarl@gmail.com <marcelaestradarl@gmail.com>
2020-10-23 18:31:05 -0600

    Api

---
+++ Laboratorio4ED2/Laboratorio4ED2/Controllers/LZWController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CompresorLZW.Estructuras;
+
+namespace Laboratorio4ED2.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class LZWController : Controller
+    {
+        [HttpPost("compress/{name}")]
+        public async Task<ActionResult> Compress([FromForm] IFormFile file, string name)
+        {
+            LZW compresor = new LZW();
+            string nombre = $"./{name}.lzw";
+
+            try
+            {
+                string cadena = System.IO.File.ReadAllText(nombre);
+                compresor.Comprimir(cadena);
+                string lineaArchivo = compresor.Archivo();
+                return StatusCode(201);
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}
--- Laboratorio4ED2/Prueba compresion/Program.cs
+++ Laboratorio4ED2/Prueba compresion/Program.cs
@@ -8,17 +8,17 @@ namespace Prueba_compresion
     {
         static void Main(string[] args)
         {
-            CompresorLZW.Estructuras.Compresor compresor = new CompresorLZW.Estructuras.Compresor();
+            //CompresorLZW.Estructuras.Compresor compresor = new CompresorLZW.Estructuras.Compresor();
             CompresorLZW.Estructuras.LZW lZW = new CompresorLZW.Estructuras.LZW();
-            //FileStream file = new FileStream(@"C:\Users\marce\Desktop\2020\Semestre II 2020\Estructura de datos II\Laboratorio\Laboratorio-4---ED2\Laboratorio4ED2\Prueba compresion\Cuesta.txt", FileMode.Open, FileAccess.Read);
-            FileStream file = new FileStream(@"C:\Users\Usuario DELL\Desktop\4to semestre 2020\Estructura de datos II\LAB\Laboratorio-4---ED2\Laboratorio4ED2\Prueba compresion\Cuesta.txt", FileMode.Open, FileAccess.Read);
+            FileStream file = new FileStream(@"C:\Users\marce\Desktop\2020\Semestre II 2020\Estructura de datos II\Laboratorio\Laboratorio-4---ED2\Laboratorio4ED2\Prueba compresion\Cuesta.txt", FileMode.Open, FileAccess.Read);
+            //FileStream file = new FileStream(@"C:\Users\Usuario DELL\Desktop\4to semestre 2020\Estructura de datos II\LAB\Laboratorio-4---ED2\Laboratorio4ED2\Prueba compresion\Cuesta.txt", FileMode.Open, FileAccess.Read);
             StreamReader lector = new StreamReader(file);
             string texto = lector.ReadToEnd();
             var diccionarioOriginal = lZW.Comprimir(texto);
 
-           // lZW.Comprimir("Lo veo bastante bien, ya casi funciona, tengo que revisarlo aun");
-            /*lZW.Comprimir("Guatemala feliz...! que tus aras no profane jamás el verdugo; ni haya esclavos que laman el yugo ni tiranos que escupan tu faz." +
-               "Si mañana tu suelo sagrado lo amenaza invasión extranjera, libre al viento tu hermosa bandera a vencer o a morir llamará.");*/
+            //lZW.Comprimir("Muy buenos dias jovenes, como estan?");
+            //lZW.Comprimir("Guatemala feliz...! que tus aras no profane jamás el verdugo; ni haya esclavos que laman el yugo ni tiranos que escupan tu faz." +
+               //"Si mañana tu suelo sagrado lo amenaza invasión extranjera, libre al viento tu hermosa bandera a vencer o a morir llamará.");
           
            string desc = lZW.Descomprimir(lZW.Archivo());
 
